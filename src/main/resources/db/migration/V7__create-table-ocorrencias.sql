75d1f67f11a6ccd8f72395fdf31266534132cf6f
Francisco Macedo <macedo270701@gmail.com>
2024-11-27 00:00:08 -0300

    refactor<ocorrenciasMigration>: edited references

--- src/main/resources/db/migration/V7__create-table-ocorrencias.sql
+++ src/main/resources/db/migration/V7__create-table-ocorrencias.sql
@@ -1,15 +1,13 @@
--- V7__create-table-ocorrencias.sql
 CREATE TABLE IF NOT EXISTS `ocorrencias` (
-  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `titulo` VARCHAR(100) NOT NULL,
   `descricao` TEXT NOT NULL,
   `tipo` ENUM('PROBLEMA_DE_INFRAESTRUTURA', 'DESENTENDIMENTO', 'SOLICITACAO_DE_MANUTENCAO', 'ASSEMBLEIA', 'DESVIO_DE_CONDUTA') NOT NULL,
   `status_aprovacao` ENUM('APROVADO', 'REJEITADO', 'AGUARDANDO') DEFAULT 'AGUARDANDO',
   `status_resolucao` ENUM('PENDENTE', 'EM_ANDAMENTO', 'CONCLUIDA') NOT NULL DEFAULT 'PENDENTE',
   `data_criacao` DATETIME NOT NULL,
-  `id_usuario` INT NOT NULL,
-  `id_area` INT,
-
-  FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
-  FOREIGN KEY (`id_area`) REFERENCES `areas` (`id`)
+  `id_usuario` BIGINT NOT NULL,
+  `id_area` BIGINT,
+  FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`),
+  FOREIGN KEY (`id_area`) REFERENCES `areas_comuns` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
